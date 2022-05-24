60dd7893c50ec4f262311f2b34e3b109c36abb59
Rabjho <rabjho@protonmail.com>
2022-05-24 14:01:06 +0200

    changed shit

--- function-invaders/Scripts/Player.gd
+++ function-invaders/Scripts/Player.gd
@@ -38,7 +38,8 @@ func start():
 	$CollisionPolygon2D.disabled = false
 
 func _on_Player_body_entered(body):
-	hide() # Player disappears after being hit.
 	emit_signal("hit")
-	# Must be deferred as we can't change physics properties on a physics callback.
+
+func _died():
 	$CollisionPolygon2D.set_deferred("disabled", true)
+	hide()
